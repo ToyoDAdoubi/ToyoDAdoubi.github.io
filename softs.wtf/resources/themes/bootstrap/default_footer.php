c9460137b74364cfffded5fa5e45b57ac11e5390
ToyoDAdoubiBackup <ToyoDAdoubiBackup@protonmail.com>
2019-05-13 21:14:16 +0800

    file index

--- softs.wtf/resources/themes/bootstrap/default_footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="container">
-  <div class="footer">
- © 2015-<?php echo date("Y")?> <a href="https://doubmirror.cf" target="_blank">逗比根据地备份</a> . All rights reserved.
-   </div>
-</footer>
